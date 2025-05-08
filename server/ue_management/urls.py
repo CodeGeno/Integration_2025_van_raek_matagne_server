61cd2ea74b5bfe53e86476edb08ba6ae302df1ec
Gregory VanRaek <vanraekgregory@hotmail.com>
2025-05-08 18:39:01 +0200

    merge

--- server/ue_management/urls.py
+++ server/ue_management/urls.py
@@ -4,12 +4,14 @@ from django.urls import path
 from ue_management.views import (
     AcademicUEListView, AcademicUEDetailView,
     LessonListView, LessonDetailView,
-    ResultListView, ResultDetailView
+    ResultListView, ResultDetailView,
+    GenerateNextYearUEsView
 )
 
 urlpatterns = [
     path('academic-ues/', AcademicUEListView.as_view(), name='ue-management_academic-ues_list'),
     path('academic-ues/<int:pk>/', AcademicUEDetailView.as_view(), name='ue-management_academic-ues_read'),
+    path('academic-ues/generate-next-year/', GenerateNextYearUEsView.as_view(), name='ue-management_generate-next-year'),
     path('lessons/', LessonListView.as_view(), name='ue-management_lessons_list'),
     path('lessons/<int:pk>/', LessonDetailView.as_view(), name='ue-management_lessons_read'),
     path('results/', ResultListView.as_view(), name='ue-management_results_list'),
